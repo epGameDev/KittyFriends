47c4e6296757643e46121f78ee74ad690bbb115a
Eric Philippot <EricpDev@gmail.com>
2024-03-26 18:07:54 -0500

    added vercell speed-insights to project and fixed some config issues

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./containers/app";
@@ -5,7 +6,9 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <SpeedInsights>
       <App />
+    </SpeedInsights>
   </React.StrictMode>,
   document.getElementById('root')
 );
