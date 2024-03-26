a229a390edf6f5469e25c0bc4eaad0ea5462fb4a
Eric Philippot <EricpDev@gmail.com>
2024-03-26 18:19:08 -0500

    ...rookie mistake, fixed vercel implementation

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,13 @@
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from "./containers/app";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SpeedInsights>
-      <App />
-    </SpeedInsights>
+    <SpeedInsights />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
